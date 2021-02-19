2b7b8779d8a696f906739b5c0b3aaf6e5a554530
Kevin Cornelius <kevco@indotim.com>
2021-02-19 13:43:01 +0700

    fix size

--- resources/views/guestlensa.blade.php
+++ resources/views/guestlensa.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-lg-12">
                     @foreach ($lensas as $lensa)
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="{{ asset('image/' . $lensa->image) }}" alt="Card image cap">
+                            <img class="card-img-top" style="max-height: 300px; max-width: 300px;" src="{{ asset('image/' . $lensa->image) }}" alt="Card image cap">
                             <div class="card-body">
                             <h5 class="card-title">{{ $lensa->name }}</h5>
                             <p>Harga lensa :  Rp. {{ $lensa->price }}</p>
